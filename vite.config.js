09e84677d982403f38ee1e810b3ea449f0091f12
jcasadomolina <jcasadomolina@outlook.es>
2024-06-03 16:07:46 +0200

    deployed website

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://jcasadomolina.github.io/MediaHive",
+  base: "https://jcasadomolina.github.io/MediaHive_Project",
 })
